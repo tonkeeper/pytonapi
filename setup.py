40723c05c0c19779ae8066dae778eea1191b0437
nessshon <shonness98@gmail.com>
2023-06-07 05:16:00 +0500

    Updated version to 0.0.4, and python requires to 3.7+

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytonapi",
-    version="0.0.3",
+    version="0.0.4",
     author="nessshon",
     description="Provide access to indexed TON blockchain.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nessshon/pytonapi/",
     packages=setuptools.find_packages(exclude="pytonapi"),
-    python_requires='>=3.10',
+    python_requires='>=3.7',
     install_requires=[
         "aiohttp>=3.8.3",
         "libscrc>=1.8.1",
@@ -20,7 +20,10 @@ setuptools.setup(
         "requests>=2.29.0",
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
