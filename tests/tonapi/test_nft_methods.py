4d35c84b30302769844b5ff7421f303260b87a85
nessshon <shonness98@gmail.com>
2023-08-10 07:28:01 +0500

    Added simple tests for v2/nfts/_bulk

--- tests/tonapi/test_nft_methods.py
+++ tests/tonapi/test_nft_methods.py
@@ -3,6 +3,11 @@ from tests.tonapi import TestTonapi
 
 ACCOUNT_ID_COLLECTION = "EQAG2BH0JlmFkbMrLEnyn2bIITaOSssd4WdisE4BdFMkZbir"  # noqa
 ACCOUNT_ID_NFT = "EQBSZKEvqoiuPUCFz-CHtpVxAwg1F8PyjZhWAJL2yeujn0_H"  # noqa
+ACCOUNT_IDS = [
+    "EQBvO9WoElkrIZF1e0l2rvPRyFVmKgqF79fdTA-qp4HNfUmW",  # noqa
+    "EQCdCF8klpqabxDq9WgezruR86-EFsHEKw2mPFg0o79f8XSR",  # noqa
+    "EQAmjob5sIWZRe9RcGhe8yMUu96WEN_BSOBEgB7vygYKOsLY",  # noqa
+]
 
 
 class TestNftMethod(TestTonapi):
@@ -26,3 +31,7 @@ class TestNftMethod(TestTonapi):
     def test_get_item_by_address(self):
         response = self.tonapi.nft.get_item_by_address(ACCOUNT_ID_NFT)
         self.assertIsInstance(response, schema.nft.NftItem)
+
+    async def test_get_bulk_items(self):
+        response = self.tonapi.nft.get_bulk_items(ACCOUNT_IDS)
+        self.assertIsInstance(response, schema.nft.NftItems)
