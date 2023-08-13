8f47b5119045c13b5ee3ef6e1583b0e68734dcff
nessshon <shonness98@gmail.com>
2023-08-13 06:29:32 +0500

    Handle transaction hash formats in v2/traces/{trace_id}

--- pytonapi/async_tonapi/methods/traces.py
+++ pytonapi/async_tonapi/methods/traces.py
@@ -1,3 +1,6 @@
+import base64
+import binascii
+
 from pytonapi.async_tonapi.client import AsyncTonapiClient
 from pytonapi.schema.traces import Trace
 
@@ -11,6 +14,9 @@ class TraceMethod(AsyncTonapiClient):
         :param trace_id: trace ID or transaction hash in hex (without 0x) or base64url format
         :return: :class:`Trace`
         """
+        if len(trace_id) == 44:
+            decoded = base64.urlsafe_b64decode(trace_id + '=' * (-len(trace_id) % 4))
+            trace_id = binascii.hexlify(decoded).decode('utf-8')
         method = f"v2/traces/{trace_id}"
         response = await self._get(method=method)
 
