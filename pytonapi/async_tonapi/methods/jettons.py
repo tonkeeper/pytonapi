ff4834f1557bf81ea1fa14e8184266fa44cef2fd
nessshon <shonness98@gmail.com>
2023-09-07 00:43:56 +0500

    Added new methods "v2/jettons/{account_id}/holders" and f"v2/jettons"

--- pytonapi/async_tonapi/methods/jettons.py
+++ pytonapi/async_tonapi/methods/jettons.py
@@ -1,5 +1,5 @@
 from pytonapi.async_tonapi.client import AsyncTonapiClient
-from pytonapi.schema.jettons import JettonInfo
+from pytonapi.schema.jettons import JettonInfo, JettonHolders, Jettons
 
 
 class JettonMethod(AsyncTonapiClient):
@@ -15,3 +15,29 @@ class JettonMethod(AsyncTonapiClient):
         response = await self._get(method=method)
 
         return JettonInfo(**response)
+
+    async def get_holders(self, account_id: str) -> JettonHolders:
+        """
+        Get jetton's holders
+
+        :param account_id: account ID
+        :return: :class:`JettonHolders`
+        """
+        method = f"v2/jettons/{account_id}/holders"
+        response = await self._get(method=method)
+
+        return JettonHolders(**response)
+
+    async def get_all_jettons(self, limit: int = 100, offset: int = 0) -> Jettons:
+        """
+        Get a list of all indexed jetton masters in the blockchain.
+
+        :param limit: Default value - 100
+        :param offset: Default value - 0
+        :return: :class:`Jettons`
+        """
+        method = f"v2/jettons"
+        params = {"limit": limit, "offset": offset}
+        response = await self._get(method=method, params=params)
+
+        return Jettons(**response)
--- pytonapi/tonapi/methods/jettons.py
+++ pytonapi/tonapi/methods/jettons.py
@@ -1,6 +1,6 @@
 from pytonapi.tonapi.client import TonapiClient
 
-from pytonapi.schema.jettons import JettonInfo
+from pytonapi.schema.jettons import JettonInfo, JettonHolders, Jettons
 
 
 class JettonMethod(TonapiClient):
@@ -16,3 +16,29 @@ class JettonMethod(TonapiClient):
         response = self._get(method=method)
 
         return JettonInfo(**response)
+
+    def get_holders(self, account_id: str) -> JettonHolders:
+        """
+        Get jetton's holders
+
+        :param account_id: account ID
+        :return: :class:`JettonHolders`
+        """
+        method = f"v2/jettons/{account_id}/holders"
+        response = self._get(method=method)
+
+        return JettonHolders(**response)
+
+    def get_all_jettons(self, limit: int = 100, offset: int = 0) -> Jettons:
+        """
+        Get a list of all indexed jetton masters in the blockchain.
+
+        :param limit: Default value - 100
+        :param offset: Default value - 0
+        :return: :class:`Jettons`
+        """
+        method = f"v2/jettons"
+        params = {"limit": limit, "offset": offset}
+        response = self._get(method=method, params=params)
+
+        return Jettons(**response)
