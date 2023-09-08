dd5990673f2a2c37f2b268776dfecb36261f5048
nessshon <shonness98@gmail.com>
2023-09-08 07:38:34 +0500

    Refactor rates.py to add a new method for fetching chart rates.

--- pytonapi/async_tonapi/methods/rates.py
+++ pytonapi/async_tonapi/methods/rates.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Optional
 
 from pytonapi.async_tonapi.client import AsyncTonapiClient
-from pytonapi.schema.rates import Rates
+from pytonapi.schema.rates import Rates, ChartRates
 
 
 class RateMethod(AsyncTonapiClient):
@@ -24,3 +24,23 @@ class RateMethod(AsyncTonapiClient):
         response = await self._get(method=method, params=params)
 
         return Rates(**response)
+
+    async def get_chart(self, token: str, currency: Optional[str] = "usd",
+                        start_date: Optional[str] = None, end_date: Optional[str] = None
+                        ) -> ChartRates:
+        """
+        Get the token price to the currency.
+
+        :param token: accept jetton master address
+        :param currency: accept fiat currency, example: "USD", "RUB" and so on
+        :param start_date: start date
+        :param end_date: end date
+        :return: :class:`ChartRates`
+        """
+        params = {'token': token, "currency": currency}
+        if start_date: params["start_date"] = start_date  # noqa:E701
+        if end_date: params["end_date"] = end_date  # noqa:E701
+        method = f"v2/rates/chart"
+        response = await self._get(method=method, params=params)
+
+        return ChartRates(**response)
