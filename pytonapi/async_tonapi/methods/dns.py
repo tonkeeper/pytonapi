8140c43902f587182ba2a9059ab77c4bdec57457
nessshon <shonness98@gmail.com>
2023-09-08 07:40:16 +0500

    Refactor dns.py to include new methods for getting domain info and auctions.

--- pytonapi/async_tonapi/methods/dns.py
+++ pytonapi/async_tonapi/methods/dns.py
@@ -1,9 +1,21 @@
 from pytonapi.async_tonapi.client import AsyncTonapiClient
-from pytonapi.schema.dns import DNSRecord
-from pytonapi.schema.domains import DomainBids
+from pytonapi.schema.dns import DNSRecord, Auctions
+from pytonapi.schema.domains import DomainBids, DomainInfo
 
 
 class DnsMethod(AsyncTonapiClient):
+
+    async def get_info(self, domain_name: str) -> DomainInfo:
+        """
+        Get full information about domain name.
+
+        :param domain_name: domain name with .ton or .t.me
+        :return: :class:`DomainInfo`
+        """
+        method = f"v2/dns/{domain_name}"
+        response = await self._get(method=method)
+
+        return DomainInfo(**response)
 
     async def resolve(self, domain_name: str) -> DNSRecord:
         """
@@ -28,3 +40,16 @@ class DnsMethod(AsyncTonapiClient):
         response = await self._get(method=method)
 
         return DomainBids(**response)
+
+    async def get_auctions(self, tld: str = "ton") -> Auctions:
+        """
+        Get all auctions.
+
+        :param tld: domain filter for current auctions "ton" or "t.me"
+        :return: :class:`Auctions`
+        """
+        method = f"v2/dns/auctions"
+        params = {"tld": tld}
+        response = await self._get(method=method, params=params)
+
+        return Auctions(**response)
--- pytonapi/tonapi/methods/dns.py
+++ pytonapi/tonapi/methods/dns.py
@@ -1,10 +1,22 @@
 from pytonapi.tonapi.client import TonapiClient
 
-from pytonapi.schema.dns import DNSRecord
-from pytonapi.schema.domains import DomainBids
+from pytonapi.schema.dns import DNSRecord, Auctions
+from pytonapi.schema.domains import DomainBids, DomainInfo
 
 
 class DnsMethod(TonapiClient):
+
+    def get_info(self, domain_name: str) -> DomainInfo:
+        """
+        Get full information about domain name.
+
+        :param domain_name: domain name with .ton or .t.me
+        :return: :class:`DomainInfo`
+        """
+        method = f"v2/dns/{domain_name}"
+        response = self._get(method=method)
+
+        return DomainInfo(**response)
 
     def resolve(self, domain_name: str) -> DNSRecord:
         """
@@ -29,3 +41,16 @@ class DnsMethod(TonapiClient):
         response = self._get(method=method)
 
         return DomainBids(**response)
+
+    def get_auctions(self, tld: str = "ton") -> Auctions:
+        """
+        Get all auctions.
+
+        :param tld: domain filter for current auctions "ton" or "t.me"
+        :return: :class:`Auctions`
+        """
+        method = f"v2/dns/auctions"
+        params = {"tld": tld}
+        response = self._get(method=method, params=params)
+
+        return Auctions(**response)
