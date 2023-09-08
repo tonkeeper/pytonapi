74363baf888f928b5bf3bc6daa2ee6e4d9a826f0
nessshon <shonness98@gmail.com>
2023-09-08 07:25:16 +0500

    Add events, staking, and storage modules to the schema package.

--- pytonapi/schema/__init__.py
+++ pytonapi/schema/__init__.py
@@ -2,9 +2,12 @@ from pytonapi.schema import accounts
 from pytonapi.schema import blockchain
 from pytonapi.schema import dns
 from pytonapi.schema import domains
+from pytonapi.schema import events
 from pytonapi.schema import jettons
 from pytonapi.schema import nft
 from pytonapi.schema import rates
+from pytonapi.schema import staking
+from pytonapi.schema import storage
 from pytonapi.schema import traces
 
 __all__ = [
@@ -12,8 +15,11 @@ __all__ = [
     "blockchain",
     "dns",
     "domains",
+    "events",
     "jettons",
     "nft",
     "rates",
+    "staking",
+    "storage",
     "traces",
 ]
