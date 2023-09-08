ad371240126fbd80104316fd10429689c81cba59
nessshon <shonness98@gmail.com>
2023-09-08 07:32:10 +0500

    Add ChartRates model to schema/rates.py.

--- pytonapi/schema/rates.py
+++ pytonapi/schema/rates.py
@@ -1,7 +1,11 @@
-from typing import Dict
+from typing import Dict, Any
 
 from pydantic.v1 import BaseModel
 
 
 class Rates(BaseModel):
     rates: Dict[str, Dict]
+
+
+class ChartRates(BaseModel):
+    points: Dict[str, Any]
