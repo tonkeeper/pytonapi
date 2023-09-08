c3932d171d293b4f79e59c07ff3b9ad4c7e5f421
nessshon <shonness98@gmail.com>
2023-09-08 07:33:01 +0500

    Update setup.py to version 0.1.0, modify python_requires to be >=3.7 and <3.11, and update aiohttp to version 3.8.5 and requests to version 2.31.0.

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytonapi",
-    version="0.0.10",
+    version="0.1.0",
     author="nessshon",
     description="Provide access to indexed TON blockchain.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nessshon/pytonapi/",
     packages=setuptools.find_packages(exclude="pytonapi"),
-    python_requires='>=3.7',
+    python_requires='>=3.7, <3.11',
     install_requires=[
-        "aiohttp>=3.8.3",
+        "aiohttp>=3.8.5",
         "libscrc>=1.8.1",
         "pydantic==2.*",
-        "requests>=2.29.0",
+        "requests>=2.31.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3.7",
