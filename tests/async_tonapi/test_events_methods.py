b7ee43140ae406cca1d564498356b77f4b74921d
nessshon <shonness98@gmail.com>
2023-10-15 21:00:37 +0500

    Refactor test_get_event methods to use event IDs in hex and base64 representations.

--- tests/async_tonapi/test_events_methods.py
+++ tests/async_tonapi/test_events_methods.py
@@ -1,11 +1,16 @@
 from pytonapi import schema
 from tests.async_tonapi import TestAsyncTonapi
 
-EVENT_ID = "53388440417dc044d00e99d89b591acc28f100332a004f180e4f14b876620c13"
+EVENT_ID_HEX = "53388440417dc044d00e99d89b591acc28f100332a004f180e4f14b876620c13"
+EVENT_ID_BASE64 = "5J+GJo3uSr36MjOwjYY+2NzYM7pvnm0WzNQktG8czbM="  # noqa
 
 
 class TestEventMethod(TestAsyncTonapi):
 
-    async def test_get_event(self):
-        response = await self.tonapi.events.get_event(EVENT_ID)
+    async def test_get_event_hex(self):
+        response = await self.tonapi.events.get_event(EVENT_ID_HEX)
+        self.assertIsInstance(response, schema.events.Event)
+
+    async def test_get_event_base64(self):
+        response = await self.tonapi.events.get_event(EVENT_ID_BASE64)
         self.assertIsInstance(response, schema.events.Event)
