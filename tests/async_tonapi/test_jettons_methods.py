1eb94f552235f3f39dd6d48d68329dba43d5f1b4
nessshon <shonness98@gmail.com>
2023-11-22 02:33:50 +0500

    Add /v2/events/{event_id}/jettons tests

--- tests/async_tonapi/test_jettons_methods.py
+++ tests/async_tonapi/test_jettons_methods.py
@@ -2,6 +2,7 @@ from pytonapi import schema
 from tests.async_tonapi import TestAsyncTonapi
 
 ACCOUNT_ID = "EQBCFwW8uFUh-amdRmNY9NyeDEaeDYXd9ggJGsicpqVcHq7B"  # noqa
+EVENT_ID = "68656e74d18b10309e41e057191abcfc42f973c82bc84326985cdbf7bf89b126"
 
 
 class TestJettonMethod(TestAsyncTonapi):
@@ -17,3 +18,8 @@ class TestJettonMethod(TestAsyncTonapi):
     async def test_get_all_jettons(self):
         response = await self.tonapi.jettons.get_all_jettons()
         self.assertIsInstance(response, schema.jettons.Jettons)
+
+    async def test_get_jetton_transfer_event(self):
+        response = await self.tonapi.jettons.get_jetton_transfer_event(EVENT_ID)
+        print(response)
+        self.assertIsInstance(response, schema.events.Event)
--- tests/tonapi/test_jettons_methods.py
+++ tests/tonapi/test_jettons_methods.py
@@ -2,6 +2,7 @@ from pytonapi import schema
 from tests.tonapi import TestTonapi
 
 ACCOUNT_ID = "EQBCFwW8uFUh-amdRmNY9NyeDEaeDYXd9ggJGsicpqVcHq7B"  # noqa
+EVENT_ID = "68656e74d18b10309e41e057191abcfc42f973c82bc84326985cdbf7bf89b126"
 
 
 class TestJettonMethod(TestTonapi):
@@ -17,3 +18,7 @@ class TestJettonMethod(TestTonapi):
     def test_get_all_jettons(self):
         response = self.tonapi.jettons.get_all_jettons()
         self.assertIsInstance(response, schema.jettons.Jettons)
+
+    def test_get_jetton_transfer_event(self):
+        response = self.tonapi.jettons.get_jetton_transfer_event(EVENT_ID)
+        self.assertIsInstance(response, schema.events.Event)
