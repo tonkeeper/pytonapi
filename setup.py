c880a268cd55cc0dae662017d54070a07142d9cc
nessshon <shonness98@gmail.com>
2024-01-09 14:53:16 +0500

    Update version to 0.1.7, update URL, exclude examples and tests, update dependencies and add support for Python 3.11 and 3.12.

--- setup.py
+++ setup.py
@@ -5,18 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytonapi",
-    version="0.1.6",
+    version="0.1.7",
     author="nessshon",
     description="Provide access to indexed TON blockchain.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nessshon/pytonapi/",
-    packages=setuptools.find_packages(),
-    python_requires='>=3.7, <3.11',
+    url="https://github.com/tonkeeper/pytonapi/",
+    packages=setuptools.find_packages(exclude=["examples", "tests"]),
     install_requires=[
-        "httpx>=0.25.1",
+        "httpx>=0.26.0",
         "websockets>=12.0",
-        "libscrc>=1.8.1",
         "pydantic==2.*",
     ],
     classifiers=[
@@ -24,6 +22,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
