2939aecd3878e400801a285ef46f87e9872081a5
nessshon <shonness98@gmail.com>
2024-02-27 02:55:49 +0500

    Update version to 0.2.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytonapi",
-    version="0.1.9",
+    version="0.2.0",
     author="nessshon",
     description="Provide access to indexed TON blockchain.",
     long_description=long_description,
