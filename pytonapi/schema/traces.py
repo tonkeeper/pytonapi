49262c46986d401994fa4311d60dcc8629ce3ad6
AndrewBalakirev <balakirev.andrei.it@gmail.com>
2024-06-18 01:22:42 +0700

    Remove unused Dict

--- pytonapi/schema/traces.py
+++ pytonapi/schema/traces.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import Optional, List, Union, Dict
+from typing import Optional, List, Union
 
 from pydantic.v1 import BaseModel
 
